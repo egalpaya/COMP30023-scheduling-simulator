c7be5f35fea2667622566b3ec05b7b7f7f71f3bc
Eishitha Galpayage Don <egalpayagedo@student.unimelb.edu.au>
2021-03-21 20:39:50 +1100

    added very basic command line arg parsing

--- allocate.c
+++ allocate.c
@@ -5,4 +5,28 @@
 /*****************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include "queue.h"
+
+int main(int argc, char *argv[]){
+
+    /*  Parse command line args */
+    int option;
+    char *filename = NULL;
+    int num_processors;
+    while ((option = getopt(argc, argv, "f:p:c")) != -1){
+        switch (option){
+            case 'f':
+                filename = optarg;
+                break;
+            case 'p':
+                num_processors = atoi(optarg);
+                break;
+            case 'c':
+                break;
+            case '?':
+                return 1;
+        }
+    }
+    
+}
