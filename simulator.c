a55616ed6bdaf1fbf8299fd2dae526ae662856ef
Eishitha Galpayage Don <egalpayagedo@student.unimelb.edu.au>
2021-03-26 16:50:53 +1100

    fixed ordering of output

--- simulator.c
+++ simulator.c
@@ -82,6 +82,7 @@ void finish_processes(pqueue_t *finished, int time, double *total_turnaround,
 
     // first loop calculates statistics and number of finished parent processes, adding 
     // finished processes to a queue
+    int i = 0;
     while ((process = (process_t *)pq_dequeue(finished))){
 
         // if the process is a subprocess, check if the parent process has remaining subprocesses
@@ -112,7 +113,8 @@ void finish_processes(pqueue_t *finished, int time, double *total_turnaround,
 
         num_finished++;
 
-        pq_enqueue(processes, process, 1, 0);
+        // counter i preserves the order of processes to be printed 
+        pq_enqueue(processes, process, 1, i++);
     }
 
     *num_proc_left -= num_finished;
